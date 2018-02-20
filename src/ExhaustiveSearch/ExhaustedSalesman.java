7f0f928e32762fe980d7816f2df2916d61075f4e
Tyler <tha7556@users.noreply.github.com>
2018-02-20 12:30:02 -0500

    Added variables: show, worst Fitness/Route

--- src/ExhaustiveSearch/ExhaustedSalesman.java
+++ src/ExhaustiveSearch/ExhaustedSalesman.java
@@ -14,22 +14,29 @@ import utility.Salesman;
 public class ExhaustedSalesman extends Salesman{
 	public long computations = 0;
 	private NumberFormat format;
-	private long startTime;
+	private long startTime, endTime;
 	/**
 	 * Creates a new Salesman out of an Array of Cities
 	 * @param cities The Array of Cities
 	 */
 	public ExhaustedSalesman(City[] cities) {
-		super(cities);
+		this(cities,true);
+	}
+	public ExhaustedSalesman(City[] cities, boolean show) {
+		super(cities,show);
 		format = NumberFormat.getNumberInstance(Locale.US);
 	}
 	/**
 	 * Begins recursively evaluating every permutation of the array of Cities
 	 */
-	public void compute() {
+	public double compute() {
 		Route r = new Route(cities);
+		computations = 0;
 		startTime = System.nanoTime();
 		next(1,r);
+		endTime = System.nanoTime();
+		return (endTime-startTime)/1000000000.0;
+		
 	}
 	/**
 	 * Private recursive method to evaluate each permutation
@@ -43,23 +50,34 @@ public class ExhaustedSalesman extends Salesman{
 			return;
 		}
 		for(int i = 1; i < route.size(); i++) {
-			if(i != current) {
+			if(current < i) {
+				if(current == 2) {
+					//System.out.println(route+"\t"+route.swap(i, current)+"\t"+i);
+				}
 				next(current+1,route.swap(i, current));
 			}
 		}
+		if(current < route.size()-2)
+			next(current+1,route);
+		
 	}
 	/**
 	 * Evaluates each Route and modifies the bestFitness and BestRoute variables 
 	 * @param route The current Route
 	 */
 	private void compareRoute(Route route) {
-		System.out.println(route);
+		//System.out.println(route);
 		double fitness = route.getFitness();
 		if(fitness > bestFitness) {
-			//System.out.println(format.format(computations)+ "   "+(System.nanoTime()-startTime)/1000000000.0 + " seconds");
+			System.out.println(Math.round((double)computations/(double)factorial(route.size())*1000000.0)/10000.0+ "%   "+(System.nanoTime()-startTime)/1000000000.0 + " seconds");
 			bestFitness = fitness;
 			bestRoute = route;
-			updateRoute(bestRoute);
+			if(show)
+				updateRoute(bestRoute);
+		}
+		if(fitness < worstFitness) {
+			worstFitness = fitness;
+			worstRoute = route;
 		}
 		computations++;
 	}
@@ -79,15 +97,13 @@ public class ExhaustedSalesman extends Salesman{
 	}
 	public static void main(String[] args) {
 		//Possibly ~17.7 hours?
-		ExhaustedSalesman man = new ExhaustedSalesman(Salesman.getFromFile("data\\TSP Test.txt"));
+		//Possible to go under 7 minutes???
+		ExhaustedSalesman man = new ExhaustedSalesman(Salesman.getFromFile("data\\TSP.txt"),false);
 		man.updateRoute(man.bestRoute);
-		System.out.println("start");
-		man.compute();
-		long end = System.nanoTime();
+		System.out.println("start\n");
+		System.out.println("Took: "+man.compute() + " seconds");
 		System.out.println("\t   "+man.computations + " loops");
 		System.out.println("Should be: " +factorial(man.getCities().length-1) + " loops");
-		double time = (end-man.startTime)/1000000000.0;
-		System.out.println("took: "+time+" seconds");
 		man.updateRoute(man.bestRoute);
 		System.out.println(man.bestRoute);
 	}
